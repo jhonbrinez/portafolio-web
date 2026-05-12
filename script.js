44d87d5c4c0a9789cc5f284bcc6afc48ceffc8b3
Jhon Alexander Briñez Loaiza <jbrinezloaiza@alumnoeseit.edu.co>
2026-05-11 23:37:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const boton = document.querySelector("button");
+
+
+boton.addEventListener("click", function(){
+
+    alert("Gracias por visitar mi hoja de vida, espero que te guste y puedas conocer un poco mas de mi, si quieres contactarme puedes hacerlo a traves de mis redes sociales o por correo electronico, gracias por tu tiempo");
+
+});
